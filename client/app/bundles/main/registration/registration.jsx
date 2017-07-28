df7798c5d55138d65fb02180644c81579ae62755
Nate Allen <neight.allen@gmail.com>
2017-07-28 13:33:01 -0600

    Removed some of the hello world stuff

--- client/app/bundles/main/registration/registration.jsx
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-
-import Player from '../components/Player';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  Player,
-});
